000ba9964572a713f423a0ab5b4961afff6bb90f
近藤ちひろ <kondochihiro@kondouchihironoMacBook-Air.local>
2026-06-24 16:56:56 +0900

    詳細画面に編集画面へ遷移するリンクを追加、削除の実装

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -36,5 +36,9 @@ class PlansController < ApplicationController
   end
 
   def destroy
+    @plan = Plan.find(params[:id])
+    @plan.destroy
+    flash[:notice] = "「#{@plan.id}」のスケジュールを削除しました"
+    redirect_to :plans
   end
 end
